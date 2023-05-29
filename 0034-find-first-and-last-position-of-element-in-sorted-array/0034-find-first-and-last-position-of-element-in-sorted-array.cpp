1ab69d92e358148c80fd0213e4d9a55d9f374317
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-05-29 19:50:31 +0530

    Time: 12 ms (39.88%), Space: 13.6 MB (79.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int startingPosition = -1, endingPosition = -1;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            if(nums[i] == target){
-                startingPosition = i;
-                break;
-            }
+    vector<int> searchRange(vector<int>& arr, int x) {
+        int n = arr.size();
+    int firstIndex=-1;
+    int start1=0;
+    int end1=n-1;
+    int mid1=start1+(end1-start1)/2;
+    while(start1<=end1){
+        if(arr[mid1]==x){
+            firstIndex=mid1;
+            end1=mid1-1;
         }
-        for(int i=n-1; i>=0; i--){
-            if(nums[i] == target){
-                endingPosition = i;
-                break;
-            }
+        else if(arr[mid1]<x){
+            start1=mid1+1;
         }
-        return {startingPosition, endingPosition};
+        else{
+            end1=mid1-1;
+        }
+        mid1=start1+(end1-start1)/2;
+    }
+
+    int lastIndex=-1;
+    int start2=0;
+    int end2=n-1;
+    int mid2=start2+(end2-start2)/2;
+    while(start2<=end2){
+        if(arr[mid2]==x){
+            lastIndex=mid2;
+            start2=mid2+1;
+        }
+        else if(arr[mid2]<x){
+            start2=mid2+1;
+        }
+        else{
+            end2=mid2-1;
+        }
+        mid2=start2+(end2-start2)/2;
+    }
+    return {firstIndex,lastIndex};
     }
 };
