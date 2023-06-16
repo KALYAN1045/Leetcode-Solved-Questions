ab4daaddaa4b95d54bf9f898a06ef373322fafe7
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-06-16 15:11:58 +0530

    Time: 7 ms (25.28%), Space: 7.6 MB (6.86%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int len=s.length();
-
-        if(len==0) return 0;
-        int i=0;
-        while(i<len and s[i]==' ') i++;
-
-        s=s.substr(i);
-
-        int sign = +1;
-        long ans=0;
-
-        if(s[0]=='-') sign=-1;
-
-        int mx=INT_MAX,mn=INT_MIN;
-
-        i = (s[0]=='+' || s[0]== '-') ? 1 : 0;
-
-        while(i<s.length()){
-
-            if(s[0]== ' ' || !isdigit(s[i])) break;
-
-            ans = ans*10 + s[i]-'0';
-
-            if(sign==-1 and -1*ans < mn) return mn;
-            if(sign==1 and ans>mx) return mx;
-
-            i++;
+    long funcatoi(string s, int i, int sign, long result){
+        if(sign*result <= INT_MIN){
+            return INT_MIN;
         }
+        if(sign*result >= INT_MAX){
+            return INT_MAX;
+        }
+        if(i >= s.size() || s[i] < '0' || s[i] > '9') return sign*result;
+        result = funcatoi(s, i+1, sign, (result*10 + (s[i]-'0')));
+        return result;
+    }
 
-        return (int)(sign*ans);
+    int myAtoi(string s) {
+        int i = 0, n = s.size(), sign = 1;
+        while (i < n && s[i] == ' ')
+            ++i;
+
+        if (s[i] == '-')
+            sign = -1, ++i;
+        else if (s[i] == '+')
+            ++i;
+
+        return funcatoi(s, i, sign, 0);
     }
 };
