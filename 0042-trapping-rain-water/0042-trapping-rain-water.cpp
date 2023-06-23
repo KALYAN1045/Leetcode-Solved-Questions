0f257bb273dc471c820461a2a1fbc42148701ea2
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-06-23 17:01:28 +0530

    Time: 19 ms (52.23%), Space: 19.7 MB (72.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& h) {
         int  n = h.size();
-        vector<int> suffmax(n, 0);
-        suffmax[n-1] = h[n-1];
-        for(int j = n - 2; j >=0; j--){
-            suffmax[j] = max(suffmax[j+1],h[j]);
-        }
-        vector<int> prefmax(n, 0);
-        prefmax[0] = h[0];
-        for(int i = 1; i < n; i++){
-            prefmax[i] = max(prefmax[i-1],h[i]);
-        }
+        int l = 0;
+        int r = n - 1;
+        int lmax = 0;
+        int rmax = 0;
         int res = 0;
-        for(int i = 0; i < n; i++){
-            int unit = min(prefmax[i], suffmax[i]) - h[i];
-            res += unit;
+        while(l < r){
+            if(h[l] <= h[r]){
+                if(h[l] > lmax){
+                    lmax = h[l];
+                }else{
+                    res += (lmax - h[l]);
+                }
+                l++;
+            }else{
+                if(h[r] > rmax){
+                    rmax = h[r];
+                }else{
+                    res += (rmax - h[r]);
+                }
+                r--;
+            }
         }
         return res;
     }
