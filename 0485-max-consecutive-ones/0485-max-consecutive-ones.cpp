5d903e266f281d41d4de4ccdcf907e233f18a333
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-06-26 16:36:14 +0530

    Time: 36 ms (88.55%), Space: 36.2 MB (48.02%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -3,16 +3,16 @@ public:
    int findMaxConsecutiveOnes(vector<int>& nums) {
     int n = nums.size();
     int i = 0;
-    int maxw = 0; // Initialize maxw to 0 instead of INT_MIN
+    int maxw = 0; 
     while (i < n) {
         if (nums[i] == 1) {
             int j = i + 1;
             while (j < n && nums[j] == 1) {
                 j++;
             }
-            int win = j - i; // Remove +1 from win calculation
+            int win = j - i;
             maxw = max(maxw, win);
-            i = j; // Remove +1 from i update
+            i = j ; 
         }
         i++;
     }
