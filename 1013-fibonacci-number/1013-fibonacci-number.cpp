0c4c45d3e4be516be5201a8745b900569a6f1bff
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-08-18 00:50:21 +0530

    Time: 2 ms (71.72%), Space: 6.2 MB (7.64%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
+    int f(int i, vector<int>&dp){
+        if(i == 0 || i == 1) return dp[i] = i;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = f(i-1,dp) + f(i-2,dp); 
+    }
     int fib(int n) {
-        if(n == 0 || n == 1) return n;
-        return fib(n-1)+fib(n-2);
+        vector<int>dp (n+1,-1);
+        return f(n,dp);
     }
 };
