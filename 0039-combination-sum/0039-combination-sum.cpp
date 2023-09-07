0923c9f852d26011f51b55d9f709141902c278e1
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-09-07 19:13:12 +0530

    Time: 38 ms (24.67%), Space: 48 MB (14.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    void combo_sum(vector<int>c, int idx, vector<vector<int>>&ans, vector<int>&v, int t){
+    void combo_sum(vector<int>c, int idx, vector<vector<int>>&ans, vector<int>&v, int&sum, int t){
         if(idx == c.size()){
-            if(t == 0){
+            if(sum == t){
                 ans.push_back(v);
             }
             return;
         }
-        if(c[idx] <= t){
+        if(sum + c[idx] <= t){
+            sum += c[idx];
             v.push_back(c[idx]);
-            combo_sum(c, idx, ans, v, t-c[idx]);
+            combo_sum(c, idx, ans, v, sum, t);
+            sum -= c[idx];
             v.pop_back();
         }
-        combo_sum(c, idx+1, ans, v, t);        
+        combo_sum(c, idx+1, ans, v, sum, t);        
     }
 
     vector<vector<int>> combinationSum(vector<int>& c, int target) {
         vector<vector<int>>ans;
         vector<int>v;
-        combo_sum(c, 0, ans, v,target);
+        int sum = 0;
+        combo_sum(c, 0, ans, v, sum, target);
         return ans;
     }
 };
+
