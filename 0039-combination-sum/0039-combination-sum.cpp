01ec494f25ca80ac949923407371a2edf390131a
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-09-15 00:09:54 +0530

    Time: 7 ms (54.89%), Space: 15.8 MB (36.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
-    void combo_sum(vector<int>c, int idx, vector<vector<int>>&ans, vector<int>&v, int&sum, int t){
-        // if(idx == c.size()){
-        //     if(sum == t){
-        //         ans.push_back(v);
-        //     }
-        //     return;
-        // }
-        if(idx >= c.size()) return;
-        if(sum == t){
+    void comb_sum(vector<int>&c, vector<vector<int>>&ans, vector<int>& v, int idx, int t){
+        if(t == 0){
             ans.push_back(v);
             return;
         }
-        if(sum + c[idx] <= t){
-            sum += c[idx];
+        if(idx >= c.size()) return;
+        comb_sum(c, ans, v, idx+1, t);
+        if(t-c[idx] >= 0){
             v.push_back(c[idx]);
-            combo_sum(c, idx, ans, v, sum, t);
-            sum -= c[idx];
+            comb_sum(c, ans, v, idx, t-c[idx]);
             v.pop_back();
         }
-        combo_sum(c, idx+1, ans, v, sum, t);        
     }
-
-    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {
         vector<vector<int>>ans;
+        sort(c.begin(),c.end());
         vector<int>v;
-        int sum = 0;
-        combo_sum(c, 0, ans, v, sum, target);
+        comb_sum(c, ans, v, 0, t);
         return ans;
     }
 };
-
