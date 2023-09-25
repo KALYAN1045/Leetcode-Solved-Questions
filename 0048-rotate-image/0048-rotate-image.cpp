e72540e40a969cccf3b09d405548cabb314936d8
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-09-25 09:53:24 +0530

    Time: 4 ms (40.36%), Space: 7.7 MB (6.35%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        for(int i=0;i<row; i++){
-            for(int j=0; j<=i;j++){
-                swap(matrix[i][j], matrix[j][i]);
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>>ans(n, vector<int>(m,0));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                ans[j][n-i-1] = mat[i][j];
             }
         }
-        for(int i=0;i<row;i++){
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
+        mat = ans;
     }
 };
