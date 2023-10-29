95ad89f413650c72e746422dfed3f81dbe73a1bb
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2023-10-29 11:24:39 +0530

    Time: 824 ms (50.37%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id AS id FROM Views
+SELECT DISTINCT author_id AS id
+FROM Views
 WHERE author_id = viewer_id
-ORDER BY id;
-
+ORDER BY id ASC;
